ace0db2078a06cb494dccdf8697933010bb0eda6
FabiolaLainez <167152904+FabiolaLainez@users.noreply.github.com>
2026-03-23 20:54:50 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-using Yarp.ReverseProxy;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+builder.Services.AddControllers();
 var app = builder.Build();
-app.MapReverseProxy();
+app.MapControllers();
 app.Run();
